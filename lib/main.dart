a4491c881779461e225e3aa035201ed82cd1dfa3
Aniket-Vyass <aniketvyas05@gmail.com>
2026-01-08 04:02:40 +0530

    Add tip and amount container

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tip Calculator',
+      title: 'uTip',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: null,
+      home: UTip(),
     );
   }
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class UTip extends StatefulWidget {
+  const UTip({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<UTip> createState() => _UTipState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _UTipState extends State<UTip> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print(context.owner.toString());
+    var theme = Theme.of(context);
+    //Add style
+    final style = theme.textTheme.titleMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
+    return Scaffold(
+      appBar: AppBar(title: const Text('uTip')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.inversePrimary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Text('Total per person', style: style),
+                Text(
+                  "\$23.89",
+                  style: style.copyWith(
+                    // color: theme.colorScheme.primary,
+                    // fontSize: theme.textTheme.displaySmall!.fontSize,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
